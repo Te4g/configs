d719f62218e91e232ae5b8e7b42f3a3372425416
Gaetan Rouseyrol <47118498+Te4g@users.noreply.github.com>
2020-11-05 01:55:52 +0100

    Update brew-install.sh

--- brew-install.sh
+++ brew-install.sh
@@ -24,3 +24,11 @@ brew cask install microsoft-teams
 brew cask install google-chrome
 brew cask install postman
 brew cask install vlc
+
+# Custom shell
+brew install starship
+touch ~/.zshrc
+echo >> eval "$(starship init zsh)"
+brew cask install font-fira-code
+brew install zsh-autosuggestions
+echo >> source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
