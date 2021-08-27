0fd52fec86a0f8f8364932bd2b9656fc2ef00781
Gaetan Rouseyrol <47118498+Te4g@users.noreply.github.com>
2021-08-28 00:48:25 +0200

    Update debian-post_install.sh

--- debian-post_install.sh
+++ debian-post_install.sh
@@ -5,6 +5,7 @@ sudo apt install \
 echo 'PATH="$PATH:/snap/bin"' >> ~/.profile ## reboot is required for it to be applied 'source ~/.profile' can temporarily do the trick
 
 sudo snap install \
-  phpstorm --classic
+  phpstorm --classic \
+  codium --classic
   
 
