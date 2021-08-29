f92dc7047454133dc20f9e6504961f0dcf2c8aad
Gaetan Rouseyrol <47118498+Te4g@users.noreply.github.com>
2021-08-29 20:57:44 +0200

    Update debian-post_install.sh

--- debian-post_install.sh
+++ debian-post_install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 sudo apt install \
-  snapd
+  snapd \
+  curl \
+  podman
 
 echo 'PATH="$PATH:/snap/bin"' >> ~/.bashrc
 
 sudo snap install \
   phpstorm --classic \
   codium --classic
-  
+
+###> DOCKER-COMPOSE RELATED ###
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+###< DOCKER-COMPOSE RELATED ###
 
